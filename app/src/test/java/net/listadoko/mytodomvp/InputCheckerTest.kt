f82c824db170efa61fff7d3216b382744ea0d8f9
ksugawara61 <katsuya@kokohadoko.net>
2020-04-29 18:56:39 +0900

    [add] change assertion using AssertJ

--- app/src/test/java/net/listadoko/mytodomvp/InputCheckerTest.kt
+++ app/src/test/java/net/listadoko/mytodomvp/InputCheckerTest.kt
@@ -1,7 +1,6 @@
 package net.listadoko.mytodomvp
 
-import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert.assertThat
+import org.assertj.core.api.Assertions.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Ignore
@@ -21,18 +20,39 @@ class InputCheckerTest {
     @Test
     fun isValid_givenLessThan3_returnsFalse() {
         val actual = target.isValid("ab")
-        assertThat(actual, `is`(false))
+        assertThat(actual).isFalse()
+    }
+
+    @Test
+    fun isValid_givenAlphabetic_returnsTrue() {
+        val actual = target.isValid("abc")
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun isValid_givenNumeric_returnsTrue() {
+        val actual = target.isValid("123")
+        assertThat(actual).isTrue()
     }
 
     @Test
     fun isValid_givenAlphaNumeric_returnsTrue() {
         val actual = target.isValid("abc123")
-        assertThat(actual, `is`(true))
+        assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun isValid_givenInvalidCharacter_returnsTrue() {
+        val actual = target.isValid("abc@123")
+        assertThat(actual).isFalse()
     }
 
 //    @Ignore("this is skip sample")
-    @Test(expected = IllegalArgumentException::class)
+    @Test
     fun isValid_givenNull_throwIllegalArgumentException() {
-        target.isValid(null)
+        assertThatExceptionOfType(IllegalArgumentException::class.java)
+            .isThrownBy { target.isValid(null) }
+            .withMessage("Cannot be null")
+            .withNoCause()
     }
 }
